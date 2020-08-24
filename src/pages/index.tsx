98a546e23727ffdd19ced5fa1c5ad2e043e1cdc5
Masterfoni <joao.lima@liferay.com>
2020-08-24 12:06:18 -0300

    Add prettier dependency integrated with eslint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 
 type Props = {
-  title: string
-}
+  title: string;
+};
 
 export default function Home({ title = 'Commerce Application' }: Props) {
   return (
@@ -65,5 +65,5 @@ export default function Home({ title = 'Commerce Application' }: Props) {
         </a>
       </footer>
     </div>
-  )
+  );
 }
