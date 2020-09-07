39eb5b4fc7d9c168c5c94f68cb074888953d563c
Masterfoni <joao.lima@liferay.com>
2020-09-07 11:55:13 -0300

    Configure styled components and apply on main

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,9 +1,18 @@
+import * as S from './styles';
 import { ReactElement } from 'react';
 
 const Main = (): ReactElement => (
-  <main>
-    <h1>{'boilerplate first test'}</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo src="/img/icon-512.png" alt="An atom symbol in a pink background" />
+    <S.Title>{'Your app is up and running!'}</S.Title>
+    <S.Description>
+      {'TypeScript, ReactJS, NextJS and Styled Components'}
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="A developer in front of a screen writing code"
+    />
+  </S.Wrapper>
 );
 
 export default Main;
--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 
 import Main from '.';
+import { EPERM } from 'constants';
 
 describe('<Main />', () => {
   it('should render', () => {
@@ -13,7 +14,15 @@ describe('<Main />', () => {
     render(<Main />);
 
     expect(
-      screen.getByRole('heading', { name: /boilerplate first test/i })
+      screen.getByRole('heading', { name: /Your app is up and running!/i })
     ).toBeInTheDocument();
+  });
+
+  it('should render the colors correctly', () => {
+    const { container } = render(<Main />);
+
+    expect(container.firstChild).toHaveStyle({
+      'background-color': '#06092b'
+    });
   });
 });
