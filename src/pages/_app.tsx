fc15be091e9789683e7ecf11f1d7190a50fa790b
Masterfoni <joaovitor_179@hotmail.com>
2020-09-21 23:13:23 -0300

    Adding PWA capabilities

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,7 @@ function App({ Component, pageProps }: AppProps): JSX.Element {
         <title>Next.js Boilerplate</title>
         <link rel="shortcut icon" href="img/icon-512.png" />
         <link rel="apple-touch-icon" href="img/icon-192.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
           content="Project template to work with TypeScript, React, NextJS and Styled Components"
